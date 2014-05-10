32dd02d536eb4da30fd0de7247449e22e9d3ce87
Lucas Constantino <lucas@taller.net.br>
2014-05-10 15:09:38 -0300

    Handlebars configuration exproter helper.

--- extend/hbs-helpers.js
+++ extend/hbs-helpers.js
@@ -10,3 +10,21 @@ var hbs = require('express-hbs');
 hbs.registerHelper('json', function(context) {
   return new hbs.handlebars.SafeString(JSON.stringify(context));
 });
+
+/**
+ * Configuration exporter.
+ */
+hbs.registerHelper('config', function(context) {
+
+  // Safely parse context.
+  var config = typeof this == 'object' ? this : {
+    data: context
+  };
+
+  // Guard blog settings, if available.
+  if (context && context.data && context.data.blog) {
+    config.blog = context.data.blog;
+  }
+
+  return new hbs.handlebars.SafeString(JSON.stringify(config));
+});
