ce78cf08b5b7d9aafa2fdd2b7c6cc637c4da5623
Lucas Constantino <lucas@taller.net.br>
2014-07-24 01:00:06 -0300

    Moved views to common directory.

--- app/blog/router.js
+++ app/blog/router.js
@@ -12,8 +12,26 @@ angular.module('blog')
     $urlRouterProvider.otherwise('/blog');
 
     $stateProvider
+
+      // Blog main route.
       .state('blog', {
         url: '/blog',
-        templateUrl: 'app/blog/views/list.html',
-      });
+        templateUrl: 'views/list.html',
+      })
+
+      // Blog post route.
+      .state('blog.post', {
+        url: '/{slug}',
+        views: {
+          '@': {
+            templateUrl: 'views/post.html'
+          }
+        }
+      })
+  })
+
+  .run(function ($rootScope, $document) {
+    $rootScope.$on('$stateChangeStart', function () {
+      $document.scrollTop(0, 500);
+    });
   });
