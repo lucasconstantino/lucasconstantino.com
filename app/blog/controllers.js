62d5b82fbca3ae6119fd6d69e085f0fe6eb07ca3
Lucas Constantino <lucas@taller.net.br>
2014-07-24 00:57:14 -0300

    Created a controller for post pages.

--- app/blog/controllers.js
+++ app/blog/controllers.js
@@ -6,6 +6,14 @@
 
 angular.module('blog')
   
+  // Blog list.
   .controller('BlogListController', function ($scope, blogPosts) {
     $scope.posts = blogPosts;
+  })
+  
+  // Blog post.
+  .controller('BlogPostController', function ($scope, $stateParams, blogPosts) {
+    $scope.post = blogPosts.filter(function (post) {
+      return post.slug == $stateParams.slug;
+    })[0] || {};
   });
