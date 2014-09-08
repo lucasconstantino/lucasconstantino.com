998e390258b1dd05407f527be0ce4cb9e935b7ac
Lucas Constantino <lucas@taller.net.br>
2014-09-07 23:21:26 -0300

    Handled deck base color.

--- app/slides/services.js
+++ app/slides/services.js
@@ -7,10 +7,13 @@
 angular.module('slides')
   
   .factory('decks', function (AppConfig) {
+
     return (AppConfig.slides.decks || []).map(function (deck) {
 
       // Transform date string into Date objects.
       deck.published = new Date(deck.published);
+
+      deck.colorRGB = deck.color && ('rgb(' + deck.color.join(',') + ')');
 
       return deck;
     });
