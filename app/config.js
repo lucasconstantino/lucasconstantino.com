ec2c0535718749a00a9a2f35437071f1594d641f
Lucas Constantino <lucas@taller.net.br>
2014-09-19 00:03:14 -0300

    Removed translation configuration from main config file.

--- app/config.js
+++ app/config.js
@@ -4,17 +4,4 @@
  * ------------------------------------------------------------------------
  */
 
-angular.module(config.project.name)
-
-  // Read translatable strings and register them for Angular.
-  .config(function ($translateProvider, $interpolateProvider) {
-
-    var userLang = (navigator.language || navigator.userLanguage || 'en').replace(/-.*/, '')
-      , translations = config.i18n || {};
-
-    Object.keys(translations).forEach(function (lang) {
-      $translateProvider.translations(lang, translations[lang]);
-    });
-
-    $translateProvider.preferredLanguage('pt');
-  });
+angular.module(config.project.name);
