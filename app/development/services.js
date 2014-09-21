da1f667cdafc2bab8f574e93e339648f84c73858
Lucas Constantino <lucas@taller.net.br>
2014-09-21 06:27:47 -0300

    Simplified dependencies service responts.

--- app/development/services.js
+++ app/development/services.js
@@ -7,6 +7,5 @@
 angular.module('development')
   
   .service('dependencies', function (AppConfig) {
-    this.node = AppConfig['package.json'].dependencies || {};
-    this.bower = AppConfig['bower.json'].dependencies || {};
+    return AppConfig.dependencies;
   });
