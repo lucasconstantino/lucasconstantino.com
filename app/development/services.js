f91aa16fd98067790e8a9cd5e2f095fe4fe342cb
Lucas Constantino <lucas@taller.net.br>
2014-09-21 23:46:57 -0300

    Parsed github pages from projects.

--- app/development/services.js
+++ app/development/services.js
@@ -7,5 +7,23 @@
 angular.module('development')
   
   .service('dependencies', function (AppConfig) {
+
+    var bower = AppConfig.dependencies.bower
+      , node = AppConfig.dependencies.node;
+
+    var overrides = {
+      'angular-translate': 'https://github.com/angular-translate/bower-angular-translate',
+      'canvas': 'https://github.com/Automattic/node-canvas'
+    };
+
+    ['bower', 'node'].forEach(function (pm) {
+      var projects = AppConfig.dependencies[pm];
+      Object.keys(projects).forEach(function (project) {
+        var parser = document.createElement('a');
+        parser.href = overrides[project] || projects[project].homepage;
+        projects[project].repo = parser.pathname.replace(/\//, '');
+      });
+    });
+
     return AppConfig.dependencies;
   });
