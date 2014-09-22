af852e420a1016d7e080f31e5404150ab327ab6b
Lucas Constantino <lucas@taller.net.br>
2014-09-21 23:49:56 -0300

    Filter non-github homepagesfrom projects

--- app/development/services.js
+++ app/development/services.js
@@ -8,9 +8,6 @@ angular.module('development')
   
   .service('dependencies', function (AppConfig) {
 
-    var bower = AppConfig.dependencies.bower
-      , node = AppConfig.dependencies.node;
-
     var overrides = {
       'angular-translate': 'https://github.com/angular-translate/bower-angular-translate',
       'canvas': 'https://github.com/Automattic/node-canvas'
@@ -19,6 +16,11 @@ angular.module('development')
     ['bower', 'node'].forEach(function (pm) {
       var projects = AppConfig.dependencies[pm];
       Object.keys(projects).forEach(function (project) {
+        if (projects[project].homepage && projects[project].homepage.indexOf('github.com') == -1) {
+          delete projects[project];
+          return;
+        }
+
         var parser = document.createElement('a');
         parser.href = overrides[project] || projects[project].homepage;
         projects[project].repo = parser.pathname.replace(/\//, '');
